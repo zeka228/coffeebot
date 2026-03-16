8fb6f1321035baddc2c2c784a172ac716e710809
LoliPain <burakznec@gmail.com>
2026-03-17 01:06:33 +0400

    implement menu keyboard

--- markups/main_markup.py
+++ markups/main_markup.py
@@ -6,12 +6,12 @@ from config import button_text
 
 class MainMenuMarkup(Markup):
     def __init__(self):
-        self.markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        self.markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         self.markup.row(types.KeyboardButton(button_text.get("menu"), style="primary"))
         self.markup.row(types.KeyboardButton(button_text.get("about")))
 
 
 class GoBackMarkup(Markup):
     def __init__(self):
-        self.markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        self.markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         self.markup.row(types.KeyboardButton(button_text.get("back")))
---
+++ markups/menu_markup.py
@@ -0,0 +1,25 @@
+from telebot import types
+
+from .markup import Markup
+
+from menu_goods import goods_data
+from config import button_text
+
+
+class MenuMarkup(Markup):
+    def __init__(self):
+        self.markup = types.InlineKeyboardMarkup(row_width=2)
+        goods_list = []
+        for i, _ in enumerate(goods_data.items()):
+            goods_list.append(types.InlineKeyboardButton(
+                text=f"{_[1][0]} {_[1][1]}руб.",
+                callback_data=f"{_[0].value}")
+            )
+        self.markup.add(*goods_list)
+        self.markup.row(types.InlineKeyboardButton(text=button_text.get("back"), callback_data="-1"))
+
+
+class MenuBack(Markup):
+    def __init__(self, back_v: bool = False):
+        self.markup = types.InlineKeyboardMarkup()
+        self.markup.row(types.InlineKeyboardButton(text=button_text.get("back"), callback_data="-1" if back_v else "-2"))
