53ffb2c593ff2be21160b20c34284eb5ce46a2ae
LoliPain <burakznec@gmail.com>
2026-03-16 21:33:19 +0400

    dev-mode

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 from os import environ
 
+BOT_VERBOSE = environ.get('COFFEE_VERBOSE', False)
 BOT_AUTH = environ.get('BOT_AUTH', '')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from telebot.async_telebot import AsyncTeleBot
 
 import routes
-from config import BOT_AUTH
+from config import BOT_AUTH, BOT_VERBOSE
 
 bot = AsyncTeleBot(BOT_AUTH)
 
@@ -13,4 +13,7 @@ bot.register_message_handler(
 )
 
 if __name__ == "__main__":
-    asyncio.run(bot.polling(skip_pending=True))
+    if BOT_VERBOSE:
+        asyncio.run(bot.polling(skip_pending=True))
+    else:
+        asyncio.run(bot.infinity_polling(skip_pending=True))
