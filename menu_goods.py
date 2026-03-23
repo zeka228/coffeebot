e5564bbbc9f3602ea0bb087a4c98b5a6bfb74a7e
LoliPain <burakznec@gmail.com>
2026-03-23 19:03:15 +0400

    implement db calls

--- menu_goods.py
+++ menu_goods.py
@@ -1,3 +1,5 @@
+import aiosqlite
+
 from typing import NamedTuple
 
 
@@ -8,8 +10,14 @@ class GoodsType(NamedTuple):
 
 
 async def get_all_goods() -> tuple[GoodsType, ...]:
-    ...
+    async with aiosqlite.connect("actual_db.db") as aiosqlite_db:
+        cur = await aiosqlite_db.execute("SELECT (id, readable_name, price) from goods")
+        goods = cur.fetchall()
+    return goods
 
 
 async def get_single(db_id: int) -> GoodsType:
-    ...
+    async with aiosqlite.connect("actual_db.db") as aiosqlite_db:
+        cur = await aiosqlite_db.execute("SELECT (id, readable_name, price) from goods WHERE id = (?)", db_id)
+        single_id = cur.fetchone()
+    return single_id
